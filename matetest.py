c47cb5db864a9d73a03eefa5f7f8ad1f59d2156f
Michael Bennett <michael.ray.bennett@gmail.com>
2013-11-14 13:39:43 -0700

    Started using textmate over idle

---
+++ matetest.py
@@ -0,0 +1,13 @@
+print ("Hello!")
+
+g = input("Select a Number: ")
+
+guess = int(g)
+
+if guess == 5:
+    print("YAY you won!")
+else:
+
+    print ("too bad!")
+        
+print ("Game Over!")
--- urllibch2.py
+++ urllibch2.py
@@ -2,7 +2,13 @@ import urllib.request
 
 page = urllib.request.urlopen("http://127.0.0.1:63517/index.html")
 text = page.read().decode("utf8")
-price = text[234:238]
+
+where = text.find('>?')
+
+start_of_price = where + 2
+end_of_price = start_of_price + 4
+
+price = text[start_of_price:end_of_price]
 
 
 print(price)
