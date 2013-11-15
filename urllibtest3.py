db672f06dd892d0d3a84229f7373d3bb440120d5
Michael Bennett <michael.ray.bennett@gmail.com>
2013-11-14 21:54:31 -0700

    use urllib w/o request

--- urllibtest3.py
+++ urllibtest3.py
@@ -1,14 +1,22 @@
 import urllib
 import time
 
-price = 99.99
+def get_price():
+	price = urllib.urlopen("http://")
+	text = page.read().decode("utf8")
+	where = text.find('>$')
+	start_of_price = text + 2
+	end_of_price = start_of_price +4
+	return(text[start_of_price:end_of_price])
+get_price()
 
-while price > 4.74:
-    time.sleep(900)
-    page = urllib.request.REQUEST("http://127.0.0.1:63517/index.html")
-    text = page.read().decode("utf8")
-    where = text.find('>$')
-    start_of_price = where + 2
-    end_of_price = start_of_price + 4
-    price = get_price()
-print ("Buy!")
+price_now = input("Do you need a price right now? (Y/N)")
+if price_now == "Y":
+    print(get_price)
+else:
+    price = 99.99
+
+    while price > 4.74:
+        time.sleep(900)
+        price = get_price()
+    print ("Buy!")
