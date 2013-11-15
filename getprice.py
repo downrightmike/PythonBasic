8ebb4e9bfa5e879de312497303a72764ccec5fc8
Michael Bennett <michael.ray.bennett@gmail.com>
2013-11-14 21:49:01 -0700

    added a get price function

---
+++ getprice.py
@@ -0,0 +1,10 @@
+import urllib 
+
+def get_price():
+	price = urllib.request.urlopen("http://")
+	text = page.read().decode("utf8")
+	where = text.find('>$')
+	start_of_price = text + 2
+	end_of_price = start_of_price +4
+	print(text[start_of_price:end_of_price])
+get_price()
