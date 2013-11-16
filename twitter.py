dd129b05c85b12a08dd2898b2ce93350f142f4a7
Michael Bennett <michael.ray.bennett@gmail.com>
2013-11-15 23:21:09 -0700

    updated test to tweet price

--- twitter.py
+++ twitter.py
@@ -1,11 +1,10 @@
 def send_to_twitter():
 	msg = "I am a msg!"
 	password_manager = urllib.HTTPPasswordMgr()
-	password_manager.add_password("Twitter API",
-										"http://twitter.com/statuses", "username", "password")
-http_handler = urllib.HTTPBasicAuthHandler(password_manager)
-page_opener = urllib.build_opener(http_handler)	
-urllib.install_opener(page_opener)
-params = urllib.parse.urlencode( {'status': msg})
-resp = urllib.urlopen("http://twitter.com/statuses/update.json", params)
-resp.read()									
+	password_manager.add_password("Twitter API", "http://twitter.com/statuses", "username", "password")
+    http_handler = urllib.HTTPBasicAuthHandler(password_manager)
+    page_opener = urllib.build_opener(http_handler)	
+    urllib.install_opener(page_opener)
+    params = urllib.parse.urlencode( {'status': msg})
+    resp = urllib.urlopen("http://twitter.com/statuses/update.json", params)
+    resp.read()									
--- urllibtest3.py
+++ urllibtest3.py
@@ -1,22 +1,33 @@
 import urllib
 import time
 
+def send_to_twitter(msg):
+	password_manager = urllib.HTTPPasswordMgr()
+	password_manager.add_password("Twitter API", "http://twitter.com/statuses", "username", "password")
+    http_handler = urllib.HTTPBasicAuthHandler(password_manager)
+    page_opener = urllib.build_opener(http_handler)	
+    urllib.install_opener(page_opener)
+    params = urllib.parse.urlencode( {'status': msg})
+    resp = urllib.urlopen("http://twitter.com/statuses/update.json", params)
+    resp.read()	
+    
 def get_price():
 	price = urllib.urlopen("http://")
 	text = page.read().decode("utf8")
 	where = text.find('>$')
 	start_of_price = text + 2
 	end_of_price = start_of_price +4
-	return(text[start_of_price:end_of_price])
+	return(text[start_of_price:end_of_price])								
+    
 get_price()
 
 price_now = input("Do you need a price right now? (Y/N)")
 if price_now == "Y":
-    print(get_price)
+    send_to_twitter(get_price())
 else:
     price = 99.99
 
     while price > 4.74:
         time.sleep(900)
         price = get_price()
-    print ("Buy!")
+    send_to_twitter("Buy!")
