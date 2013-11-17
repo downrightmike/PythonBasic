10025006ed57250dd54686d46bae7511206d0284
Michael Bennett <michael.ray.bennett@gmail.com>
2013-11-16 21:39:10 -0700

    need to pay attention to str

--- surfscores.py
+++ surfscores.py
@@ -1,13 +1,17 @@
 scores = []
+names = []
 result_f = open("results.txt")
 for line in result_f:
-	(name, score) = line.split()
-	acores.append(float(score))
+ (name, score) = line.split()
+ scores.append(float(score))
+ names.append(name)
 result_f.close()
 scores.sort()
 scores.reverse()
+names.sort()
+names.reverse()
 print("Top scores are: ")
-print(scores[0])
-print(scores[1])
-print(scores[2])
+print(names[0] + ' ' + str(scores[0]))
+print(names[1] + ' ' + str(scores[1]))
+print(names[2] + ' ' + str(scores[2]))
 
